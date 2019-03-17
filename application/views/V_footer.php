1435589f7e15adda7235a06398570adc7c950807
Novalia Widya Popy Kristanti <novalia.kristanti@sigma.co.id>
2019-03-17 23:20:45 +0700

    change template

--- application/views/V_footer.php
+++ application/views/V_footer.php
@@ -1,235 +1,41 @@
-	<!-- ****** Instagram Area Start ****** -->
-	<div class="instargram_area owl-carousel section_padding_100_0 clearfix" id="portfolio">
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/1.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/2.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/3.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/4.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/5.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/6.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/1.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Instagram Item -->
-		<div class="instagram_gallery_item">
-			<!-- Instagram Thumb -->
-			<img src="<?php echo base_url(); ?>assets/img/instagram-img/2.jpg" alt="">
-			<!-- Hover -->
-			<div class="hover_overlay">
-				<div class="yummy-table">
-					<div class="yummy-table-cell">
-						<div class="follow-me text-center">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
+<div class="footer py-4">
+	<div class="container-fluid">
+		<p>
+			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+		</p>
 	</div>
-	<!-- ****** Our Creative Portfolio Area End ****** -->
+</div>
 
-	<!-- ****** Footer Social Icon Area Start ****** -->
-	<div class="social_icon_area clearfix">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="footer-social-area d-flex">
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i><span>facebook</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i><span>GOOGLE+</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i><span>linkedin</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i><span>VIMEO</span></a>
-						</div>
-						<div class="single-icon">
-							<a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i><span>YOUTUBE</span></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- ****** Footer Social Icon Area End ****** -->
 
-	<!-- ****** Footer Menu Area Start ****** -->
-	<footer class="footer_area">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="footer-content">
-						<!-- Logo Area Start -->
-						<div class="footer-logo-area text-center">
-							<a href="index.html" class="yummy-logo">Yummy Blog</a>
-						</div>
-						<!-- Menu Area Start -->
-						<nav class="navbar navbar-expand-lg">
-							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#yummyfood-footer-nav" aria-controls="yummyfood-footer-nav" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars" aria-hidden="true"></i> Menu</button>
-							<!-- Menu Area Start -->
-							<div class="collapse navbar-collapse justify-content-center" id="yummyfood-footer-nav">
-								<ul class="navbar-nav">
-									<li class="nav-item active">
-										<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="#">Features</a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="#">Categories</a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="#">Archive</a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="#">About</a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="#">Contact</a>
-									</li>
-								</ul>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
 
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<!-- Copywrite Text -->
-					<div class="copy_right_text text-center">
-						<p>Copyright @2018 All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
 
-	<!-- ****** Footer Menu Area End ****** -->
 
-	<!-- Jquery-2.2.4 js -->
-	<script src="<?php echo base_url(); ?>assets/js/jquery/jquery-2.2.4.min.js"></script>
-	<!-- Popper js -->
-	<script src="<?php echo base_url(); ?>assets/js/bootstrap/popper.min.js"></script>
-	<!-- Bootstrap-4 js -->
-	<script src="<?php echo base_url(); ?>assets/js/bootstrap/bootstrap.min.js"></script>
-	<!-- All Plugins JS -->
-	<script src="<?php echo base_url(); ?>assets/js/others/plugins.js"></script>
-	<!-- Active JS -->
-	<script src="<?php echo base_url(); ?>assets/js/active.js"></script>
+</div>
+
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery-ui.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.stellar.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.countdown.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/swiper.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/aos.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/picturefill.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/lightgallery-all.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.mousewheel.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+<script>
+	$(document).ready(function(){
+		$('#lightgallery').lightGallery();
+	});
+</script>
+
 </body>
+</html>
--- application/views/V_header.php
+++ application/views/V_header.php
@@ -1,23 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Album &mdash; Colorlib Website Template</title>
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-	<!-- Title -->
-	<title>Yummy Blog - Food Blog Template</title>
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300i,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/fonts/icomoon/style.css">
 
-	<!-- Favicon -->
-	<link rel="icon" href="<?php echo base_url(); ?>assets/img/core-img/favicon.ico">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery-ui.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lightgallery.min.css">    
 
-	<!-- Core Stylesheet -->
-	<link href="<?php echo base_url(); ?>assets/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker.css">
 
-	<!-- Responsive CSS -->
-	<link href="<?php echo base_url(); ?>assets/css/responsive/responsive.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/fonts/flaticon/font/flaticon.css">
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/swiper.css">
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/aos.css">
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+
+	<style type="text/css">
+		.btn{
+			border-radius: 5px;
+		}
+	</style>
 
 </head>
+<body>
+	<div class="site-wrap">
+
+		<div class="site-mobile-menu">
+			<div class="site-mobile-menu-header">
+				<div class="site-mobile-menu-close mt-3">
+					<span class="icon-close2 js-menu-toggle"></span>
+				</div>
+			</div>
+			<div class="site-mobile-menu-body"></div>
+		</div>
+
+
+
+
+		<header class="site-navbar py-3" role="banner">
+
+			<div class="container-fluid">
+				<div class="row align-items-center">
+
+					<div class="col-6 col-xl-2" data-aos="fade-down">
+						<h1 class="mb-0"><a href="<?php echo base_url('Home'); ?>" class="text-black h2 mb-0">logo</a></h1>
+					</div>
+					<div class="col-10 col-md-8 d-none d-xl-block" data-aos="fade-down">
+						<nav class="site-navigation position-relative text-right text-lg-center" role="navigation">
+
+							<ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+								<li class="active"><a href="<?php echo base_url('Home'); ?>">Home</a></li>
+								<!-- 		<li class="has-children"> -->
+									<li>
+										<a href="<?php echo base_url('Category'); ?>">Category</a>
+					<!-- 				<ul class="dropdown">
+										<li><a href="#">Nature</a></li>
+										<li><a href="#">Portrait</a></li>
+										<li><a href="#">People</a></li>
+										<li><a href="#">Architecture</a></li>
+										<li><a href="#">Animals</a></li>
+										<li><a href="#">Sports</a></li>
+										<li><a href="#">Travel</a></li>
+										<li class="has-children">
+											<a href="#">Sub Menu</a>
+											<ul class="dropdown">
+												<li><a href="#">Menu One</a></li>
+												<li><a href="#">Menu Two</a></li>
+												<li><a href="#">Menu Three</a></li>
+											</ul>
+										</li>
+									</ul> -->
+								</li>
+								<li><a href="services.html">Services</a></li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+
+					<div class="col-6 col-xl-2 text-right" data-aos="fade-down">
+						<div class="d-none d-xl-inline-block">
+			<!-- 				<ul class="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-class="social">
+								<li>
+									<a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+								</li>
+								<li>
+									<a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+								</li>
+								<li>
+									<a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+								</li>
+								<li>
+									<a href="#" class="pl-3 pr-3"><span class="icon-youtube-play"></span></a>
+								</li>
+							</ul> -->
+							<a href="<?php echo base_url('Login'); ?>" class="btn text-white btn-secondary"><i class="fa fa-arrow-circle-left"></i> Login
+							</a>
+						</div>
+
+						<div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+					</div>
+				</div>
+			</div>
+		</header>
